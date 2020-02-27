c184618b98ca6d1fdc66e400c48b39996caec836
zhouchao-z <1051386008@qq.com>
2020-02-27 15:00:19 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express");
-var global = require("./config");
+// var global = require("./config");
 var loader = require("./loader");
 var app = new express;
-app.use(express.static(global["page_path"]));
-app.listen(global["port"]);
+app.use(express.static("./page"));
+app.listen(12306);
 
 app.get("/insertChat", loader.get("/insertChat"));
 app.get("/queryChat", loader.get("/queryChat"));
